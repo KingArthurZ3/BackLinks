d7a2a67107345e148bb72bdffa3eabcce3744a2d
Arthur Zhang <arthur@Arthurs-MacBook-Pro.local>
2017-11-05 15:50:40 -0800

    added window and fixed top nav

--- js/folder.js
+++ js/folder.js
@@ -1,3 +1,10 @@
-$(function() {
+$(document).ready(function(){
     $(".folder").draggable({ containment: "parent" });
+    
+    $(".window").draggable({ containment: "parent" });
+    
+    //TODO add in click for window
 });
+
+
+    
