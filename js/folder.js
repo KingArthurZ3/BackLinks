56f1523c5f6ef4b75f0ead147b02199c44774d7f
mattzh72 <mzhou133@student.fuhsd.org>
2017-11-05 16:25:37 -0800

    Window CSS Updates

--- js/folder.js
+++ js/folder.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
     $(".folder").draggable({ containment: "parent" });
     
     $(".window").draggable({ containment: "parent" });
+    $(".window").resizable({
+      maxHeight: 600,
+      maxWidth: 600,
+      minHeight: 200,
+      minWidth: 200
+    });
     
     //TODO add in click for window
 });
