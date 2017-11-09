e87099772b0558ec65967be6f2f5902a7193ac49
mattzh72 <mzhou133@student.fuhsd.org>
2017-11-08 18:49:24 -0800

    Added window functionality

--- js/window.js
+++ js/window.js
@@ -1,23 +1,77 @@
 $(document).ready(function () {
     let ogHeight = $(".window").css("height");
+    let ogWidth = $(".window").css("width");
+
+    $(".window").draggable({
+        containment: "parent",
+        handle: ".top-bar",
+    });
+
+    $(".window").resizable({
+        maxHeight: 600,
+        maxWidth: 800,
+        minHeight: 300,
+        minWidth: 200
+    });
 
     $(".window-minimize").on("click", function () {
-        if ($(".window").css("height") != "25px"){
+        if ($(".window").css("height") != "25px") {
+            ogHeight = $(".window").css("height");
+            ogWidth = $(".window").css("width");
+            
             $(".window").animate({
                 height: "25"
+            }, {
+                duration: 500,
+                specialEasing: {
+                    width: "linear",
+                    height: "easeInSine"
+                }
+            });
+            
+            $(".window").animate({
+                width: "200"
+            }, {
+                duration: 100,
+                specialEasing: {
+                    width: "linear",
+                    height: "easeInSine"
+                }
+            });
+            
+            setTimeout(function(){
+                $(".top-bar").css("border-radius", "6px 6px 6px 6px");
             }, 500);
 
-             $(".window").resizable( 'disable' );
+            $(".window").resizable('disable');
         }
     });
 
     $(".window-maximize").on("click", function () {
-        if ($(".window").css("height") == "25px"){
+        if ($(".window").css("height") == "25px") {
+            $(".top-bar").css("border-radius", "6px 6px 0px 0px");
+            
+            $(".window").animate({
+                width: ogWidth
+            }, {
+                duration: 100,
+                specialEasing: {
+                    width: "linear",
+                    height: "easeInSine"
+                }
+            });
+            
             $(".window").animate({
                 height: ogHeight
-            }, 500);
+            }, {
+                duration: 500,
+                specialEasing: {
+                    width: "linear",
+                    height: "easeInSine"
+                }
+            });
 
-            $(".window").resizable( 'enable' );
+            $(".window").resizable('enable');
         }
     });
 
