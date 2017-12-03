74e1d6b8eb3d045aa99bf93d36835f95de95ac70
Arthur Zhang <arthur@Arthurs-MacBook-Pro.local>
2017-12-02 17:00:00 -0800

    fixed json parser and search feature

--- js/annoucements.js
+++ js/annoucements.js
@@ -1,13 +1,9 @@
 //'[{ "title": "test 1", "author": "a1", "date": "1.1.1111", "body": "this is test 1 for all intended purposes", "tags": "announcements, 1, specifical" }, { "title": "test 2", "author": "a2", "date": "2.2.2222", "body": "this is test 2 essentially", "tags": "announcements, 2, technical" }, { "title": "test 3", "author": "a3", "date": "3.3.3333", "body": "this is test 3 basically", "tags": "announcements, 3, general" }]'
 // announcements is saved to a json file ex: alert(jsonResponse[0].title) should return test 1y
-let announcements = [];
+var announcements = [];
 let currentlyDisplayedRowID;
 
 $(document).ready(function () {
-
-    //Matt's stuff
-    displayRows(announcements);
-
 
     var options = {
         shouldSort: true,
@@ -24,7 +20,27 @@ $(document).ready(function () {
             "tags",
         ]
     };
-    let fuse = new Fuse(announcements, options); // "list" is the item array
+    
+    //downloads file and gets length using ajax, synchronous so you will have access to all file data
+    var json = (function () {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': 'https://firebasestorage.googleapis.com/v0/b/backlinksnpo.appspot.com/o/announcements%2Freal.json?alt=media&token=f0b35b47-00cb-4490-b4e6-b3164afcd5fb',
+            'dataType': "json",
+            'success': function (data) {
+                json = data;
+            }
+        });
+        return json;
+    })(); 
+    // gets length of json array
+    var length = Object.keys(json).length;
+    
+    displayRows(json);
+    
+    let fuse = new Fuse(json, options); // "list" is the item array
 
     $(".side-row-input").focus(function () {
         displayRows(fuse.search(""));
@@ -41,7 +57,7 @@ $(document).ready(function () {
         let author = $(this).find(".side-row-author");
         let date = $(this).find(".side-row-date");
 
-        let entry = findEntryByRow(title.text(), author.text(), date.text(), announcements);
+        let entry = findEntryByRow(title.text(), author.text(), date.text(), json);
 
         if (entry) {
             displayEntry(entry);
@@ -61,27 +77,11 @@ $(document).ready(function () {
 
     $(".side-row-input").focusout(function () {
         if ($(".side-row-input").val() == "") {
-            displayRows(announcements);
+            displayRows(json);
         }
     });
-    
-    //downloads file and gets length using ajax, synchronous so you will have access to all file data
-    var json = (function () {
-        var json = null;
-        $.ajax({
-            'async': false,
-            'global': false,
-            'url': 'https://firebasestorage.googleapis.com/v0/b/backlinksnpo.appspot.com/o/announcements%2Freal.json?alt=media&token=f0b35b47-00cb-4490-b4e6-b3164afcd5fb',
-            'dataType': "json",
-            'success': function (data) {
-                json = data;
-            }
-        });
-        return json;
-    })(); 
-    // gets length of json array
-    var length = Object.keys(json).length;
-    
+
+//    alert("the length of json is" + length + " and the title in entry one is: " + json[1].title)
     
 });
 
@@ -92,9 +92,9 @@ function findEntryByRow(title, author, date, jsonArr) {
 
     for (let i = 0; i < jsonArr.length; i++) {
         let entry = jsonArr[i];
-        jsonTitle = entry["title"].replace(/\s/g, '');
-        jsonAuthor = entry["author"].replace(/\s/g, '');
-        jsonDate = entry["date"].replace(/\s/g, '');
+        jsonTitle = entry.title.replace(/\s/g, '');
+        jsonAuthor = entry.author.replace(/\s/g, '');
+        jsonDate = entry.date.replace(/\s/g, '');
 
         if (title == jsonTitle && author == jsonAuthor && date == jsonDate) {
             return entry;
@@ -104,6 +104,7 @@ function findEntryByRow(title, author, date, jsonArr) {
     return null;
 }
 
+// displays the update on the right side
 function displayEntry(entry, parentDiv = ".side-content-pane") {
     $(parentDiv).empty();
     $(parentDiv).append('<div class="title"></div> <div class="entry-date"></div> <div class="entry-author"></div> <div class="entry-content"> </div> <div class="entry-tags"></div>');
@@ -114,15 +115,15 @@ function displayEntry(entry, parentDiv = ".side-content-pane") {
     $(".entry-tags").text("Tags: " + entry["tags"]);
 }
 
+// displays the updates on the left
 function displayRows(jsonArr, parentDiv = '.side-scroll-pane-rows') {
     $(parentDiv).empty();
     let html = "";
-
-    for (let i = 0; i < jsonArr.length; i++) {
+    for(let i = 0; i < jsonArr.length; i++) {
         let entry = jsonArr[i];
-        jsonTitle = entry["title"].replace(/\W/g, '');
-        jsonAuthor = entry["author"].replace(/\W/g, '');
-        jsonDate = entry["date"].replace(/\W/g, '');
+        jsonTitle = entry.title.replace(/\W/g, '');
+        jsonAuthor = entry.author.replace(/\W/g, '');
+        jsonDate = entry.date.replace(/\W/g, '');
 
         html += '<div class="side-row" id="';
         html += jsonTitle + jsonAuthor + jsonDate;
@@ -155,23 +156,3 @@ function clearHighlight(rowID) {
     $(rowID).css("background-color", "rgba(237, 237, 237, 0)");
     $(rowID).css("box-shadow", "0px 0px 15px rgba(0, 0, 0, 0)");
 }
-
-
-
-//    gsReference.getDownloadURL().then(function(url) {
-//        // This can be downloaded directly:
-//        var xhr = new XMLHttpRequest();
-//        xhr.responseType = 'text';
-//        xhr.onload = function(event) {
-//            text = xhr.responseText;
-//            jsonResponse = JSON.parse(text);
-//            // all code that uses the json responses should go here!
-//            var length = Object.keys(jsonResponse).length;
-//            $('.entry-content').text(jsonResponse[length-1].body);
-//            $('.title').text(jsonResponse[length-1].title);
-//
-//        };
-//        xhr.open('GET', url);
-//        xhr.send();
-//
-//    });
